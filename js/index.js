ba4eb17cbf0176c0d037abdc7025ddf4a94a7464
Kevin Ross <kevin.ross@alienfast.com>
2016-04-19 10:01:59 -0500

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 //import 'babel-polyfill/dist/polyfill'
 //import 'babel-polyfill'
 //import 'core-js/es6/symbol'
+//export * from 'babel-polyfill'
 
 export {default as Datepicker} from './datepicker'
 //export * from './templates'
