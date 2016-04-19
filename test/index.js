d0ae46eb252ea829fe6357600e8076bfd53847dd
Kevin Ross <kevin.ross@alienfast.com>
2016-04-19 10:01:29 -0500

    don't need the Buffer global with external chai

--- test/index.js
+++ test/index.js
@@ -1,12 +1,4 @@
-//var Buffer = Buffer ? Buffer : {}
-//import {Buffer as bf} from 'buffer'
-//var Buffer = bf
-//export {Buffer} from 'buffer'
-
-
-//import '../js/index'
 import './setup/ES2015'
 import './setup/ES5'
-
 
 import './datepicker/methods'
