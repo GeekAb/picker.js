2d19a03b96531d7fd747a01cccff8e15ea5dc408
Kevin Ross <kevin.ross@alienfast.com>
2016-05-04 17:26:55 -0500

    Better solution for #13, eliminates the need for the additional script tag.  https://github.com/rollup/rollup/issues/641#issuecomment-217016841

--- js/index.js
+++ js/index.js
@@ -4,11 +4,12 @@
 //export * from 'babel-polyfill'
 
 export {default as Datepicker} from './datepicker'
-//export * from './templates'
-
 export {default as BS3Template} from './templates/bs4Template'
 export {default as BS4Template} from './templates/bs4Template'
 export {default as BMD4Template} from './templates/bmd4Template'
 export {default as StringTemplate} from './templates/stringTemplate'
 
-export {default as moment} from 'moment'
+// export moment for use with additional locales #13
+import moment from 'moment'
+window.moment = moment
+export { moment }
